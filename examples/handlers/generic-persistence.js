107cfa73d7bf205695242c5b9aab86cf41447e1e
Vitaly Vengrov <vengrov@gmail.com>
2016-03-19 08:24:59 +0200

    fix (example) add missing target to handler

--- examples/handlers/generic-persistence.js
+++ examples/handlers/generic-persistence.js
@@ -2,9 +2,6 @@
 
 'use strict';
 
-Backendless.ServerCode.Persistence.afterCreate(function(req, res) {
-    console.log('afterCreate');
-
-    //stop further operation proceeding and respond to the client with a specific result
-    return { foo: 'bar' };
+Backendless.ServerCode.Persistence.afterCreate('*', function(req, res) {
+    console.log('afterCreate:generic');
 });
