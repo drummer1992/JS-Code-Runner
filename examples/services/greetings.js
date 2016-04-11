c1b4286f1df8c121000ecd2596a0d309be7ba035
Vitaly Vengrov <vengrov@gmail.com>
2016-04-11 16:53:28 +0300

    feat (services) add configuration items

--- examples/services/greetings.js
+++ examples/services/greetings.js
@@ -3,14 +3,29 @@
 'use strict';
 
 class GreetingsService {
-
   /**
    * @param {String} userName
    * @returns {string}
    */
   getGreeting(userName) {
-    return `Hello ${userName}`;
+    return `${this.config.lang} ${userName}`;
   }
 }
+
+GreetingsService.configItems = [
+  {
+    name        : 'lang',
+    displayName : 'Language',
+    required    : true,
+    type        : 'boolean',
+    defaultValue: 'English',
+    options     : ['English', 'German', 'Spanish'],
+    hint        : 'Please select a Greetings Language'
+  }
+];
+
+// GreetingsService.configItems = Backendless.ServerCode.ServiceConfigBuilder()
+//   .addChoise('lang', 'Language', true, ['English', 'German', 'Spanish'], 'English', 'Please select a Greetings Language')
+//   .build();
 
 Backendless.ServerCode.addService(GreetingsService);
