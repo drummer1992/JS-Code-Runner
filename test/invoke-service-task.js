86c6a6741738dc24fcbe61da8bcd3a33309d96b2
Vitaly Vengrov <vengrov@gmail.com>
2016-04-19 16:23:55 +0300

    fix (invoke-service) handle async throwing
    - more informative error messages during service execution
    - more unit tests for invoke-service task

--- test/invoke-service-task.js
+++ test/invoke-service-task.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const ServerCode      = require('../lib/server-code/api'),
-      ServerCodeModel = require('../lib/server-code/model'),
-      assert          = require('assert'),
-      executor        = require('../lib/server-code/runners/tasks/executor'),
-      executionResult = require('../lib/server-code/runners/tasks/util/result-wrapper').executionResult,
-      argsUtil        = require('../lib/server-code/runners/tasks/util/args');
+const assert    = require('assert'),
+      TestModel = require('./support/test-model'),
+      executor  = require('../lib/server-code/runners/tasks/executor'),
+      argsUtil  = require('../lib/server-code/runners/tasks/util/args');
 
 require('mocha');
 
@@ -13,8 +11,8 @@ function createTask(service, method, args, configItems) {
   return {
     ___jsonclass        : executor.RSI,
     initAppData         : {},
-    serviceId           : service.name,
-    className           : service.name,
+    serviceId           : service,
+    className           : service,
     method              : method,
     applicationId       : '',
     relativePath        : '',
@@ -27,37 +25,30 @@ function createTask(service, method, args, configItems) {
 
 function invoke(task, model) {
   return executor.execute(task, { backendless: { repoPath: '' } }, model)
-    .then(res => res && JSON.parse(res));
-}
-
-function modelStub(service) {
-  const result = new ServerCodeModel();
-  result.definitions.addFile = () => {
-  };
-  result.addService(service);
-
-  return result;
+    .then(res => JSON.parse(res))
+    .then(res => res[0]);
 }
 
 describe('[invoke-service] task executor', function() {
   it('should invoke service method', function() {
     let invoked = false;
 
-    class Foo {
-      bar() {
+    class Service {
+      test() {
         invoked = true;
       }
     }
 
-    return invoke(createTask(Foo, 'bar'), modelStub(Foo))
+    return invoke(createTask(Service.name, 'test'), new TestModel().addService(Service))
+      .then(res => assert.equal(res, null))
       .then(() => assert.ok(invoked));
   });
 
   it('should pass arguments to service method', function() {
     let invoked = false;
 
-    class Foo {
-      bar(a, b, c) {
+    class Service {
+      test(a, b, c) {
         assert.equal(a, 1);
         assert.equal(b, 2);
         assert.deepEqual(c, {});
@@ -66,8 +57,113 @@ describe('[invoke-service] task executor', function() {
       }
     }
 
-    return invoke(createTask(Foo, 'bar', [1, 2, {}]), modelStub(Foo))
-      .then(() => assert.ok(invoked));
+    return invoke(createTask(Service.name, 'test', [1, 2, {}]), new TestModel().addService(Service))
+      .then(res => {
+        assert.equal(res, null);
+        assert(invoked);
+      });
+  });
+
+  describe('should handle error', function() {
+    it('when service not found', function() {
+      return invoke(createTask('Test', 'test'), new TestModel())
+        .then(res => assert.equal(res.exceptionMessage, '[Test] service does not exist'));
+    });
+
+    it('when service method not found', function() {
+      class Test {
+      }
+
+      return invoke(createTask('Test', 'test'), new TestModel().addService(Test))
+        .then(res => assert.equal(res.exceptionMessage,
+          '[test] method does not exist in [Test] service or is not a function'));
+    });
+
+    it('when service method throws an error', function() {
+      class Test {
+        test() {
+          throw new Error('Oops');
+        }
+      }
+
+      return invoke(createTask('Test', 'test'), new TestModel().addService(Test))
+        .then(res => assert.equal(res.exceptionMessage, 'Oops'));
+    });
+
+    it('when service method rejects promise', function() {
+      class Test {
+        test() {
+          return Promise.reject('Oops');
+        }
+      }
+
+      return invoke(createTask('Test', 'test'), new TestModel().addService(Test))
+        .then(res => assert.equal(res.exceptionMessage, 'Oops'));
+    });
+
+    it('when service method produces async error behind promise', function() {
+      class Test {
+        test() {
+          return new Promise(() => {
+            process.nextTick(() => {
+              throw new Error('Oops');
+            });
+          });
+        }
+      }
+
+      return invoke(createTask('Test', 'test'), new TestModel().addService(Test))
+        .then(res => assert.equal(res.exceptionMessage, 'Oops'));
+    });
+  });
+
+  it('should transform arguments if custom types are defined', function() {
+    let invoked = false;
+
+    class Foo {
+    }
+    class Bar {
+      constructor() {
+        this.baz = {};
+      }
+    }
+
+    class Baz {
+    }
+
+    class Service {
+      test(foo, bar, baz) {
+        assert(foo instanceof Foo, 'Invalid type casting');
+        assert(bar instanceof Bar, 'Invalid type casting');
+        assert(bar.baz instanceof Baz, 'Invalid deep type casting');
+        assert(baz[0] instanceof Baz, 'Invalid array elements type casting');
+        assert(baz[1] instanceof Baz, 'Invalid array elements type casting');
+
+        invoked = true;
+      }
+    }
+
+    const model = new TestModel()
+      .addService(Service, {
+        methods: {
+          test: {
+            params: [
+              { name: 'foo', type: { name: 'Foo' } },
+              { name: 'boo', type: { name: 'Bar' } },
+              { name: 'baz', type: { name: 'Array', elementType: { name: 'Baz' } } }
+            ]
+          }
+        }
+      })
+      .addType(Foo)
+      .addType(Bar, { properties: { baz: { type: { name: 'Baz' } } } })
+      .addType(Baz);
+
+    return invoke(createTask(Service, 'test', [new Foo(), new Bar(), [new Baz(), new Baz()]]), model)
+      .then((res) => {
+        assert.equal(res, null);
+        assert(invoked);
+      });
   });
 
 });
---
+++ test/support/test-model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const ServerCodeModel = require('../../lib/server-code/model');
+
+class ServerCodeTestModel extends ServerCodeModel {
+  constructor() {
+    super();
+    this.definitions.files = ['test'];
+  }
+
+  addService(service, definition) {
+    super.addService(service, 'test');
+
+    return this.addDefinition(service.name, definition);
+  }
+
+  addType(type, definition) {
+    super.addType(type, 'test');
+
+    return this.addDefinition(type.name, definition);
+  }
+
+  addDefinition(name, definition) {
+    if (definition) {
+      this.definitions.types[name] = definition;
+    }
+
+    return this;
+  }
+}
+
+module.exports = ServerCodeTestModel;
