1e8c96400ee621b9c3a5b56fd30d6f4714cde408
Vitaly Vengrov <vengrov@gmail.com>
2016-04-21 18:14:56 +0300

    fix (parse-service) several errors related to model parsing
    - add unit tests for parse-service task

---
+++ test/parse-service.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const assert   = require('assert'),
+      executor = require('../lib/server-code/runners/tasks/executor');
+
+require('backendless').ServerCode = require('../lib/server-code/api');
+require('mocha');
+
+function createTask(path) {
+  return {
+    ___jsonclass : executor.RAI,
+    initAppData  : {},
+    actionType   : 'PARSE_CUSTOM_SERVICE_FROM_JAR',
+    applicationId: '',
+    relativePath : 'test',
+    argObject    : {
+      fileName: path
+    }
+  };
+}
+
+function invoke(task) {
+  return executor.execute(task, { backendless: { repoPath: '' } })
+    .then(res => JSON.parse(res));
+}
+
+describe('[parse-service] task executor', function() {
+  it('should parse found services', function() {
+    return invoke(createTask('fixtures'))
+      .then((res) => {
+        assert.equal(res.exception, null);
+        assert.deepEqual(res.arguments, [{
+          'configuration' : [],
+          'description'   : 'ShoppingCartService',
+          'name'          : 'ShoppingCartService',
+          'version'       : '0.0.0',
+          'xmlDescription': '<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<namespaces>\n  <namespace name=\"services\" fullname=\"services\">\n    <service name=\"ShoppingCartService\" fullname=\"services.ShoppingCartService\">\n      <method name=\"addItem\" type=\"void\" nativetype=\"void\" fulltype=\"void\" javatype=\"void\">\n        <arg name=\"cartName\" type=\"String\" nativetype=\"String\" fulltype=\"String\" javatype=\"java.lang.String\"/>\n        <arg name=\"item\" type=\"ShoppingItem\" nativetype=\"services.ShoppingItem\" fulltype=\"services.ShoppingItem\" javatype=\"services.ShoppingItem\"/>\n      </method>\n      <method name=\"addItems\" type=\"void\" nativetype=\"void\" fulltype=\"void\" javatype=\"void\">\n        <arg name=\"cartName\" type=\"String\" nativetype=\"String\" fulltype=\"String\" javatype=\"java.lang.String\"/>\n        <arg name=\"items\" type=\"Array\" nativetype=\"List&lt;services.ShoppingItem&gt;\" fulltype=\"Array\" javatype=\"java.util.List&lt;services.ShoppingItem&gt;\" elementType=\"ShoppingItem\"/>\n      </method>\n      <method name=\"purchase\" type=\"Order\" nativetype=\"services.Order\" fulltype=\"services.Order\" javatype=\"services.Order\">\n        <arg name=\"cartName\" type=\"String\" nativetype=\"String\" fulltype=\"String\" javatype=\"java.lang.String\"/>\n      </method>\n    </service>\n    <datatype name=\"ShoppingItem\" fullname=\"services.ShoppingItem\" typeNamespace=\"services\">\n      <field name=\"objectId\" type=\"String\" nativetype=\"String\" fulltype=\"String\" javatype=\"java.lang.String\"/>\n      <field name=\"product\" type=\"String\" nativetype=\"String\" fulltype=\"String\" javatype=\"java.lang.String\"/>\n      <field name=\"price\" type=\"Number\" nativetype=\"float\" fulltype=\"Number\" javatype=\"float\"/>\n      <field name=\"quantity\" type=\"Number\" nativetype=\"float\" fulltype=\"Number\" javatype=\"float\"/>\n    </datatype>\n    <datatype name=\"Order\" fullname=\"services.Order\" typeNamespace=\"services\">\n      <field name=\"items\" type=\"Array\" nativetype=\"List&lt;services.ShoppingItem&gt;\" fulltype=\"Array\" javatype=\"java.util.List&lt;services.ShoppingItem&gt;\" elementType=\"ShoppingItem\"/>\n      <field name=\"orderPrice\" type=\"Number\" nativetype=\"float\" fulltype=\"Number\" javatype=\"float\"/>\n    </datatype>\n  </namespace>\n  <runtime generationMode=\"FULL\">\n  </runtime>\n</namespaces>'
+        }]);
+      });
+  });
+
+  it('should handle no services', function() {
+    return invoke(createTask('dummy-folder'))
+      .then((res) => {
+        assert.equal(res.exception, null);
+        assert.deepEqual(res.arguments, []);
+      });
+  });
+});
