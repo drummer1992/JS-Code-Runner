227def765dd99ca07fc24609ed840e52c0a5f506
Vitaly Vengrov <vengrov@gmail.com>
2016-04-25 14:08:06 +0300

    doc (handlers) add user.afterRegister example

---
+++ examples/handlers/user.js
@@ -0,0 +1,10 @@
+/* global Backendless */
+
+'use strict';
+
+Backendless.enablePromises();
+Backendless.ServerCode.User.afterRegister((req, res) => {
+  if (res.result) {
+    return Backendless.UserService.assignRole(res.result.email, 'MyCustomRole');
+  }
+});
