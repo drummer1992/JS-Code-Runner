ae16a137eccb619cd4e8df50450caff7e9874074
Vitaly Vengrov <vengrov@gmail.com>
2016-08-17 15:01:38 +0300

    doc (services) add more comments for shopping-cart service example

--- examples/handlers/custom/average-stars.js
+++ examples/handlers/custom/average-stars.js
@@ -2,9 +2,11 @@
 
 'use strict';
 
-// This is an example of custom event handler which accepts a string 'movie' parameter,
-// and, using the Backendless SDK, retrieves the 'Review' table data,
-// calculates the average star rating for the passed movie and returns it to the caller
+/***
+ * This is an example of custom event handler which accepts a string 'movie' parameter,
+ * and, using the Backendless SDK, retrieves the 'Review' table data,
+ * calculates the average star rating for the passed movie and returns it to the caller
+ */
 
 Backendless.enablePromises();
 Backendless.ServerCode.customEvent('movieRating', request => {
@@ -18,18 +20,16 @@ Backendless.ServerCode.customEvent('movieRating', request => {
       const totalReviews = reviews.data.length;
       const totalStars = reviews.data.reduce((memo, review) => memo + review.stars, 0);
 
-      //use console.log to debug your server code
+      // use console to debug your server code
       console.log(`${totalReviews} reviews found with ${totalStars} stars in total`);
 
       if (totalReviews === 0) {
-        throw new Error(`No reviews found for [${movie}] movie` );
+        throw new Error(`No reviews found for [${movie}] movie`);
       }
 
       return totalStars / totalReviews;
     },
 
-    //You can omit this part if you don't want to wrap a lookup error into a specific error message
-    //Also it is redundant to print the error using console.log for debug because the Code Runner will do this anyway
     err => {
       console.error(err);
 
